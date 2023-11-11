94eadbcc5c42ba08d4529252a72d66195997f9c5
Julien Thomas <julthoma@3A4.42angouleme.fr>
2023-11-11 09:48:10 +0100

    update

--- born2beroot_install.sh
+++ born2beroot_install.sh
@@ -9,75 +9,144 @@ fi
 read -p "Enter the desired username: " USERNAME
 
 # Update system
-echo "Updating the system..."
-apt update -y
-apt upgrade -y
+read -p "Update the system ? (y/n): " ACTION
+
+if [ "$ACTION" == "y" ]; then
+	echo "Executing system update..."
+	apt update -y
+	apt upgrade -y
+	echo "System updated."
+elif [ "$ACTION" == "n" ]; then
+	echo "Skipping system update."
+fi
 
 # Install sudo
 apt install sudo -y
 
 # Setup users and groups
-echo "Setup users and groups and adding $USERNAME to sudo users..."
-adduser $USERNAME sudo
-echo "$USERNAME    ALL=(ALL) ALL" | sudo tee -a "/etc/sudoers"
-groupadd user42
-sudo usermod -aG user42 $USERNAME
+read -p "Setup users and groups and adding $USERNAME to sudo users ? (y/n): " ACTION
+
+if [ "$ACTION" == "y" ]; then
+	echo "Executing users and groups setup..."
+	adduser $USERNAME sudo
+	echo "$USERNAME	ALL=(ALL) ALL" | sudo tee -a "/etc/sudoers"
+	groupadd user42
+	sudo usermod -aG user42 $USERNAME
+	echo "Users and groups setup completed."
+elif [ "$ACTION" == "n" ]; then
+	echo "Skipping users and groups setup."
+fi
 
 # Install git
-echo "Install git..."
-apt update -y
-apt upgrade -y
-apt install git -y
+read -p "Install git ? (y/n): " ACTION
+
+if [ "$ACTION" == "y" ]; then
+	echo "Installing git..."
+	apt install git -y
+	echo "Git installed."
+elif [ "$ACTION" == "n" ]; then
+	echo "Skipping git installation."
+fi
 
 # Install wget and vim
-echo "Install wget and vim..."
-apt install wget -y
-apt install vim -y
+read -p "Install wget and vim ? (y/n): " ACTION
+
+if [ "$ACTION" == "y" ]; then
+	echo "Installing wget and vim..."
+	apt install wget -y
+	apt install vim -y
+	echo "wget and vim installed."
+elif [ "$ACTION" == "n" ]; then
+	echo "Skipping wget and vim installation."
+fi
 
 # Install Oh my zsh
-echo "Install Oh my zsh..."
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+read -p "Install Oh my zsh ? (y/n): " ACTION
+
+if [ "$ACTION" == "y" ]; then
+	echo "Installing Oh my zsh..."
+	sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+	echo "Oh my zsh installed."
+elif [ "$ACTION" == "n" ]; then
+	echo "Skipping Oh my zsh installation."
+fi
 
 # Install and configure openssh-server
-echo "Setup ssh server..."
-apt update -y
-apt install openssh-server -y
-service ssh restart
-sed -i 's/#Port 22/Port 4242/' /etc/ssh/sshd_config
-service ssh restart
+read -p "Setup ssh server ? (y/n): " ACTION
+
+if [ "$ACTION" == "y" ]; then
+	echo "Installing and configuring ssh server..."
+	apt update -y
+	apt install openssh-server -y
+	service ssh restart
+	sed -i 's/#Port 22/Port 4242/' /etc/ssh/sshd_config
+	service ssh restart
+	echo "ssh server installed and configured."
+elif [ "$ACTION" == "n" ]; then
+	echo "Skipping ssh server installation and configuration."
+fi
 
 # Install and configure ufw
-echo "Setup firewall..."
-apt install ufw -y
-ufw reset
-ufw enable
-ufw allow proto tcp to 0.0.0.0/0 port 4242
-ufw allow 80 # Allow HTTP traffic if needed
+read -p "Setup firewall ? (y/n): " ACTION
+
+if [ "$ACTION" == "y" ]; then
+	echo "Installing and configuring firewall..."
+	apt install ufw -y
+	ufw reset
+	ufw enable
+	ufw allow proto tcp to 0.0.0.0/0 port 4242
+	ufw allow 80 # Allow HTTP traffic if needed
+elif [ "$ACTION" == "n" ]; then
+	echo "Skipping firewall installation and configuration."
+fi
+
+
 
 
 # Install password quality checking library (libpam-pwquality)
-echo "Install libpam-pwquality..."
-apt install libpam-pwquality
-echo "minlen=10" | sudo tee -a "/etc/pam.d/common-password"
-sed -i 's/password    requisite         pam_pwquality.so retry=3/password    requisite         pam_pwquality.so retry=3 lcredit =-1 ucredit=-1 dcredit=-1 maxrepeat=3 usercheck=0 difok=7 enforce_for_root/' /etc/pam.d/common-password
-sed -i 's/PASS_MAX_DAYS*9999/PASS_MAX_DAYS 30/' /etc/login.defs
-sed -i 's/PASS_MIN_DAYS*0/PASS_MIN_DAYS 2/' /etc/login.defs
-#echo "Defaults  env_reset" | sudo tee -a "/etc/sudoers"
-#echo "Defaults  mail_badpass" | sudo tee -a "/etc/sudoers"
-#echo 'Defaults  secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin' | sudo tee -a "/etc/sudoers"
-echo 'Defaults  badpass_message="Password is wrong, please try again!"' | sudo tee -a "/etc/sudoers"
-mkdir /var/log/sudo
-echo 'Defaults  logfile="/var/log/sudo/sudo.log"' | sudo tee -a "/etc/sudoers"
-echo "Defaults  log_input, log_input" | sudo tee -a "/etc/sudoers"
-echo "Defaults  requiretty" | sudo tee -a "/etc/sudoers"
-echo "$USERNAME ALL=(ALL) NOPASSWD: /usr/local/bin/monitoring.sh" | sudo tee -a "/etc/sudoers"
+read -p "Install libpam-pwquality ? (y/n): " ACTION
+
+if [ "$ACTION" == "y" ]; then
+	echo "Installing libpam-pwquality..."
+	apt install libpam-pwquality -y
+	echo "minlen=10" | sudo tee -a "/etc/pam.d/common-password"
+
+	#TODO: check if the following lines are correct
+
+	sed -i 's/password    requisite         pam_pwquality.so retry=3/password    requisite         pam_pwquality.so retry=3 lcredit =-1 ucredit=-1 dcredit=-1 maxrepeat=3 usercheck=0 difok=7 enforce_for_root/' /etc/pam.d/common-password
+	sed -i 's/PASS_MAX_DAYS*9999/PASS_MAX_DAYS 30/' /etc/login.defs
+	sed -i 's/PASS_MIN_DAYS*0/PASS_MIN_DAYS 2/' /etc/login.defs
+	#echo "Defaults	env_reset" | sudo tee -a "/etc/sudoers"
+	#echo "Defaults	mail_badpass" | sudo tee -a "/etc/sudoers"
+	#echo 'Defaults	secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin' | sudo tee -a "/etc/sudoers"
+	echo 'Defaults	badpass_message="Password is wrong, please try again!"' | sudo tee -a "/etc/sudoers"
+	mkdir /var/log/sudo
+	echo 'Defaults	logfile="/var/log/sudo/sudo.log"' | sudo tee -a "/etc/sudoers"
+	echo "Defaults	log_input, log_input" | sudo tee -a "/etc/sudoers"
+	echo "Defaults	requiretty" | sudo tee -a "/etc/sudoers"
+	echo "$USERNAME ALL=(ALL) NOPASSWD: /usr/local/bin/monitoring.sh" | sudo tee -a "/etc/sudoers"
+	echo "libpam-pwquality installed."
+elif [ "$ACTION" == "n" ]; then
+	echo "Skipping libpam-pwquality installation."
+fi
 
 # Setup crontab script
-echo "Setup monitoring script with crontab"
-apt install -y net-tools
-crontab -l > crontmp
-echo "*/10 * * * * /usr/local/bin/monitoring.sh" >> crontmp
-crontab crontmp
-rm crontmp
+read -p "Setup monitoring script with crontab ? (y/n): " ACTION
+
+if [ "$ACTION" == "y" ]; then
+	echo "Setting up monitoring script with crontab..."
+	apt install -y net-tools
+	crontab -l > crontmp
+	echo "*/10 * * * * /usr/local/bin/monitoring.sh" >> crontmp
+	crontab crontmp
+	rm crontmp
+	rm crontmp
+	#TODO: add /usr/local/bin/monitoring.sh
+	echo "Monitoring script setup completed."
+elif [ "$ACTION" == "n" ]; then
+	echo "Skipping monitoring script setup."
+fi
+
+
 
 echo "Installation completed."
